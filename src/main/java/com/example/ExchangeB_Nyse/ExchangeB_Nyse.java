75d09956069d7cf4fa5d6d93b8a6f4314f6608d6
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-06-18 19:33:13 +0530

    Initial commit

---
+++ src/main/java/com/example/ExchangeB_Nyse/ExchangeB_Nyse.java
@@ -0,0 +1,13 @@
+package com.example.ExchangeB_Nyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeB_Nyse {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeB_Nyse.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ExchangeB_Nyse/CareEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExchangeB_Nyse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
