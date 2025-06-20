d9de71ef28eac5aa429970d95fe9fcf74c49e73e
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-06-20 08:43:29 +0530

    Initial commit

--- src/main/java/com/example/ExchangeB_Nyse/Config/WebSocketConfig.java
+++ src/main/java/com/example/ExchangeB_Nyse/Config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/nyse").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/ws/nyse").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
